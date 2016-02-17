c0cac7c2106241f5381e497dfd328423d5e2d148
hys2015 <markheng2014@gmail.com>
2016-02-17 17:08:32 +0800

    hello window

---
+++ LearnOpenGL/src/hellowindow.cpp
@@ -0,0 +1,73 @@
+//GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+//GLFW
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to Create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        //rendering command
+        GLfloat red, green, blue;
+        red = 0.8;
+        green = 0.0;
+        blue = 0.0;
+
+        glClearColor(red, green, blue, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //swap buffer avoid flert-prob
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+
+
+    return 0;
+}
+
+//key-press-callback function
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    std::cout << key << std::endl;
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
