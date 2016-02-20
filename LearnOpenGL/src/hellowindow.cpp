bd1c3ed824f67d05bedfc391587b2137439d570e
hys2015 <markheng2014@gmail.com>
2016-02-21 00:26:56 +0800

    coordinate system exercise 3 solution

--- LearnOpenGL/src/hellowindow.cpp
+++ LearnOpenGL/src/hellowindow.cpp
@@ -229,17 +229,15 @@ int main()
             glm::mat4 model, view, projection;
             model = glm::translate(model, cubePositions[i]);
             GLfloat angle = 20.0f * i;
-            model = glm::rotate(model, glm::radians((float)glfwGetTime()*angle), glm::vec3(0.5f, 1.0f, 0.0f));
+            if (i % 3 == 0){
+                angle *= glfwGetTime();
+            }
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
             view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
             projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
             glUniformMatrix4fv(glGetUniformLocation(shader.ProgramID, "model"), 1, GL_FALSE, glm::value_ptr(model));
             glUniformMatrix4fv(glGetUniformLocation(shader.ProgramID, "view"), 1, GL_FALSE, glm::value_ptr(view));
             glUniformMatrix4fv(glGetUniformLocation(shader.ProgramID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-
-
-            //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-            //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-            //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
         glBindVertexArray(0);
